6ef8abc0b9172255a19a5fb22870bde5dfb21f05
addiner <azeddine103@hotmail.com>
2026-03-19 10:49:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,10 @@
-const CACHE_NAME = 'pro-calc-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'calc-v3';
+const assets = ['./', './index.html', './manifest.json'];
 
-// Installation: Dateien in den Cache laden
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// Strategie: Erst im Cache suchen, dann Netzwerk (Offline-First)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
